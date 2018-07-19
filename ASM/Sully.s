e6fcb969fcf8cacae26e5e070aac28c8b10ef9e5
ryaoi <ryaoi@student.42.fr>
2018-07-19 17:39:30 +0200

    all good

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -1,7 +1,7 @@
 section .data
 sullysource: db "Sully_%d.s", 00
-sysstr: db "gcc -Wall -Wextra -Werror Sully_%d.s -o Sully_%d", 00
-str: db "section .data%csullysource: db %cSully_%%d.s%c, 00%csysstr: db %cgcc -Wall -Wextra -Werror Sully_%%d.s -o Sully_%%d%c, 00%cstr: db %c%s%c, 00%c%%define FLAG_OPT 0x601%c%%define SYSOPEN 0x2000005%cglobal _main%cextern _dprintf%csection .text%c%%macro start_point 0%c;first comment%c_main:%cpush rbp%cmov rbp, rsp%csub rsp, 0x150%cxor rcx, rcx%c%%%%fill_newline:%cmov dword [rsp+rcx*8], 0x0A%cinc rcx%ccmp rcx, 42%cjne %%%%fill_newline%cmov rax, SYSOPEN%clea rdi, [rel gracekid]%cmov rsi, FLAG_OPT%cmov rdx, 0o644%csyscall%cmov rdi, rax%clea rsi, [rel str]%cmov rdx, 0x0A%cmov rcx, 0x22%cmov r8, 0x22%cmov r9, 0x0A%cmov dword [rsp], 0x22%clea rax, [rel str]%cmov [rsp+0x8], rax%cmov dword [rsp+0x10], 0x22%ccall _dprintf%cadd rsp, 0x150%cpop rbp%cret%c%%endmacro%cstart_point%c", 00
+sysstr: db "nasm -fmacho64 Sully_%d.s;gcc -Wall -Wextra -Werror Sully_%d.o -o Sully_%d", 00
+str: db "section .data%csullysource: db %cSully_%%d.s%c, 00%csysstr: db %cnasm -fmacho64 Sully_%%d.s;gcc -Wall -Wextra -Werror Sully_%%d.o -o Sully_%%d%c, 00%cstr: db %c%s%c, 00%c%%define FLAG_OPT 0x601%c%%define SYSOPEN 0x2000005%cglobal _main%cextern _dprintf%cextern _asprintf%cextern _system%cextern _free%cextern _close%csection .text%c_main:%cpush rbp%cmov rbp, rsp%csub rsp, 0x1000%cmov qword [rbp-0x4], %d%ccreate_sullys:%clea rdi, [rbp - 0x30]%clea rsi, [rel sullysource]%cmov rdx, [rbp-0x4]%cxor al, al%cpush rdx%cpush rdi%ccall _asprintf%cpop rdi%cpop rdx%cmov qword [rbp-0x4], rdx%cmov rax, SYSOPEN%cmov qword rdi, [rbp-0x30]%cmov rsi, FLAG_OPT%cmov rdx, 0o644%csyscall%ccmp rax, -1%cje end_sully%cmov [rbp-0x40], rax%cmov qword rdi, [rbp-0x30]%ccall _free%cxor rcx, rcx%cfill_newline:%cmov dword [rsp+rcx*8], 0x0A%cinc rcx%ccmp rcx, 400%cjne fill_newline%cmov rdi, [rbp-0x40]%clea rsi, [rel str]%cmov rdx, 0x0A%cmov rcx, 0x22%cmov r8, 0x22%cmov r9, 0x0A%cmov dword [rsp], 0x22%cmov dword [rsp+0x8], 0x22%cmov dword [rsp+0x10], 0x0A%cmov dword [rsp+0x18], 0x22%clea rbx, [rel str]%cmov qword [rsp+0x20], rbx%cmov dword [rsp+0x28], 0x22%cmov eax, [rbp-0x4]%cmov dword [rsp+0xA0], eax%ccall _dprintf%cmov rdi, [rbp-0x40]%ccall _close%cmov eax, [rbp-0x4]%cdec eax%ccmp eax, 0%cjl end_sully%clea rdi, [rbp-0x30]%clea rsi, [rel sysstr]%cmov rdx, [rbp-0x4]%cmov rcx, [rbp-0x4]%cmov r8, [rbp-0x4]%cxor al, al%ccall _asprintf%cmov qword rdi, [rbp-0x30]%ccall _system%cmov qword rdi, [rbp-0x30]%ccall _free%cmov eax, [rbp-0x4]%cdec eax%cmov [rbp-0x4], eax%ccmp eax, 0%cjge create_sullys%cend_sully:%cadd rsp, 0x1000%cpop rbp%cxor rax, rax%cret%c", 00
 %define FLAG_OPT 0x601
 %define SYSOPEN 0x2000005
 global _main
@@ -9,6 +9,7 @@ extern _dprintf
 extern _asprintf
 extern _system
 extern _free
+extern _close
 section .text
 _main:
 push rbp
@@ -33,6 +34,7 @@ mov rdx, 0o644
 syscall
 cmp rax, -1
 je end_sully
+mov [rbp-0x40], rax
 mov qword rdi, [rbp-0x30]
 call _free
 xor rcx, rcx
@@ -41,7 +43,7 @@ mov dword [rsp+rcx*8], 0x0A
 inc rcx
 cmp rcx, 400
 jne fill_newline
-mov rdi, [rbp-0x10]
+mov rdi, [rbp-0x40]
 lea rsi, [rel str]
 mov rdx, 0x0A
 mov rcx, 0x22
@@ -54,15 +56,20 @@ mov dword [rsp+0x18], 0x22
 lea rbx, [rel str]
 mov qword [rsp+0x20], rbx
 mov dword [rsp+0x28], 0x22
+mov eax, [rbp-0x4]
+mov dword [rsp+0xA0], eax
 call _dprintf
+mov rdi, [rbp-0x40]
+call _close
 mov eax, [rbp-0x4]
 dec eax
 cmp eax, 0
 jl end_sully
 lea rdi, [rbp-0x30]
 lea rsi, [rel sysstr]
-mov rcx, [rbp-0x4]
 mov rdx, [rbp-0x4]
+mov rcx, [rbp-0x4]
+mov r8, [rbp-0x4]
 xor al, al
 call _asprintf
 mov qword rdi, [rbp-0x30]
@@ -79,4 +86,3 @@ add rsp, 0x1000
 pop rbp
 xor rax, rax
 ret
-
