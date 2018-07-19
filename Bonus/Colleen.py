eeb3ff1e90e28a9340a8fba418b2cbd515bde088
ryaoi <ryaoi@student.42.fr>
2018-07-19 19:55:14 +0200

    1 bonus

---
+++ Bonus/Colleen.py
@@ -0,0 +1,13 @@
+def nothing():
+    pass
+"""
+welcome to python
+"""
+if __name__ == "__main__":
+    """
+    welcome to main
+    """
+    nothing()
+    colleen_str = """def nothing():%c    pass%c%c%c%c%cwelcome to python%c%c%c%c%cif __name__ == %c__main__%c:%c    %c%c%c%c    welcome to main%c    %c%c%c%c    nothing()%c    colleen_str = %c%c%c%s%c%c%c%c    print((colleen_str %% (chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), '"', '"', chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', colleen_str, '"', '"', '"', chr(0x0A), chr(0x0A))))%c"""
+    print((colleen_str % (chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), '"', '"', chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', colleen_str, '"', '"', '"', chr(0x0A), chr(0x0A))))
+
