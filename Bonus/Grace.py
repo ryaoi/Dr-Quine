51d198c13a1cc59f619be272d82493ad3a6592a9
ryaoi <ryaoi@student.42.fr>
2018-07-20 13:12:56 +0200

    2nd bonus done

---
+++ Bonus/Grace.py
@@ -0,0 +1,11 @@
+def f():
+    """
+    Comment!
+    """
+    if __name__ == "__main__":
+        grace = """def f():%c    %c%c%c%c    Comment!%c    %c%c%c%c    if __name__ == %c__main__%c:%c        grace = %c%c%c%s%c%c%c%c        try:%c            with open(%cGrace_kid.py%c, %cw%c) as kid: kid.write((grace %% (chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), '"', '"', chr(0x0A), '"', '"', '"', grace, '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', '"', chr(0x0A), chr(0x0A), chr(0x0A), chr(0x0A))))%c        except:%c            pass%cf()%c"""
+        try:
+            with open("Grace_kid.py", "w") as kid: kid.write((grace % (chr(0x0A), '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', chr(0x0A), '"', '"', chr(0x0A), '"', '"', '"', grace, '"', '"', '"', chr(0x0A), chr(0x0A), '"', '"', '"', '"', chr(0x0A), chr(0x0A), chr(0x0A), chr(0x0A))))
+        except:
+            pass
+f()
